b4144bedc1db53f6c68730e5571891593e1282c3
Gabriel Abubakar <gabrielogrima@gmail.com>
2023-09-10 01:20:00 +0100

    features implemented

--- app.js
+++ app.js
@@ -4,16 +4,36 @@ const port = 3000
 
 
 app.get('/', (req, res) => {
+    // Set the information included in query parameters as variables
+    const slack_name = req.query.slack_name;
+    const track = req.query.track;
+
     // Set the Content-Type header to indicate JSON response
     res.setHeader('Content-Type', 'application/json');
 
-    const jsonResponse = {
-        message: 'Hello, Express!',
-        date: new Date()
-    };
+    // Throw an error if query parameters arent included in the GET request
+    if (!slack_name || !track) {
+        return res.status(400).json({ error: 'Both slack_name and track parameters are required.' });
+    }
+
+    // Calculate the day of the week
+    var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var dt = new Date()
+    var day = dt.getDay()
+
+    // Create the response object
+    const jsonResponse2 = {
+        "slack_name": slack_name,
+        "current_day": week[day],
+        "utc_time": new Date().toISOString(),
+        "track": track,
+        "github_file_url": "https://github.com/GabrielAbubakar/backend-hngx-stage-one/blob/master/app.js",
+        "github_repo_url": "https://github.com/GabrielAbubakar/backend-hngx-stage-one",
+        "status_code": 200
+    }
 
 
-    res.send(jsonResponse)
+    res.send(jsonResponse2)
 })
 
 app.listen(port, () => {
